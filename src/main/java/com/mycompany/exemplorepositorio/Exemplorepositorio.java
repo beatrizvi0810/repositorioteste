dcec222a0d952402abefaaa3f8d409a020595a0b
BEATRIZ VITORIA GOMES DE OLIVEIRA <beatriz.07928@aluno.pr.senac.br>
2023-09-05 16:24:34 -0300

    primeira alteração

--- src/main/java/com/mycompany/exemplorepositorio/Exemplorepositorio.java
+++ src/main/java/com/mycompany/exemplorepositorio/Exemplorepositorio.java
@@ -11,6 +11,6 @@ package com.mycompany.exemplorepositorio;
 public class Exemplorepositorio {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(" esta alterado");
     }
 }
