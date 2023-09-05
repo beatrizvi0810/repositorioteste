7b4fd370b55bcc3bb799a88f17a1304094ac7671
BEATRIZ VITORIA GOMES DE OLIVEIRA <beatriz.07928@aluno.pr.senac.br>
2023-09-05 15:45:16 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/exemplorepositorio/Exemplorepositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplorepositorio;
+
+/**
+ *
+ * @author beatriz.07928
+ */
+public class Exemplorepositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
